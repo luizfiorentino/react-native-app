ab171ba6b98efc024ab064d1ef3475a90221cc46
luizfiorentino <lgfiorentino@gmail.com>
2022-05-17 13:51:16 +0200

    reactive app feat/ scrollView added to render the images

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// src/App.js
+import React, { useRef } from "react";
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  Image,
+  Button,
+  Alert,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const randomNum = useRef(Math.random()).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+        Hello React Native
+      </Text>
+      <ScrollView>
+        <ActivityIndicator
+          size="large"
+          color="#c1262c"
+          style={{ marginBottom: 30 }}
+        />
+        {[0, 1, 2, 3, 4].map((i) => {
+          return (
+            <Image
+              key={i}
+              source={{
+                uri: `https://picsum.photos/500/300?random=${randomNum + i}`,
+              }}
+              style={{ width: "100%", height: 160, marginBottom: 30 }}
+            />
+          );
+        })}
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "black",
+            padding: 20,
+            marginBottom: 30,
+          }}
+        >
+          <Text>Hello again!</Text>
+        </View>
+        <View>
+          <Button
+            onPress={() => Alert.alert("Learning RN is so easy")}
+            title="Learn More"
+            color="#c1262c"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
